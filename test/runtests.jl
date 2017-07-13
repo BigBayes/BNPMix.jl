c459e65dd7657d2c90b7005d8c2b6dacf80cd6b6
Emile Mathieu <emilem@bamlab.fr>
2017-07-13 22:29:28 +0100

    add cluster test in runtets

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,4 @@ using Base.Test
 @testset "metropolis"          begin include("metropolisTest.jl")          end
 @testset "sliceFiniteInterval" begin include("sliceFiniteIntervalTest.jl") end
 @testset "sliceStepOut"        begin include("sliceStepOutTest.jl")        end
+@testset "cluster"             begin include("clusterTest.jl")             end
