e2e5c0ca30c71bcc433864bb574285680a33e125
Emile Mathieu <emilem@bamlab.fr>
2017-07-17 14:47:57 +0100

    add new test to runtests list

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
 using NRMMM
 using Base.Test
 
-@testset "metropolis"          begin include("metropolisTest.jl")          end
-@testset "sliceFiniteInterval" begin include("sliceFiniteIntervalTest.jl") end
-@testset "sliceStepOut"        begin include("sliceStepOutTest.jl")        end
-@testset "cluster"             begin include("clusterTest.jl")             end
-@testset "mixture"             begin include("mixtureTest.jl")             end
+@testset "metropolis"             begin include("metropolisTest.jl")             end
+@testset "sliceFiniteInterval"    begin include("sliceFiniteIntervalTest.jl")    end
+@testset "sliceStepOut"           begin include("sliceStepOutTest.jl")           end
+@testset "cluster"                begin include("clusterTest.jl")                end
+@testset "mixture"                begin include("mixtureTest.jl")                end
+@testset "NormalGammaIndependent" begin include("NormalGammaIndependentTest.jl") end
